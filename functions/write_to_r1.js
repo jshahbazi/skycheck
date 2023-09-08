1456afb2ba8ba5a869d80d6308ab0ba6be3dd472
John Shahbazian <john.shahbazian@ceros.com>
2023-09-08 12:29:48 -0700

    Test write to r1

--- functions/write_to_r1.js
+++ functions/write_to_r1.js
@@ -1,43 +1,33 @@
 export const onRequestPost = async ({ request, env, ctx }) => {
   const dataToSave = await request.json();
-  // return new Response(JSON.stringify({ dataToSave }));
-  let result = null;
 
-  // try {
- 
-
-    result = await env.SKYCHECK_DB.prepare(
-      `insert into images (bucket, path, mimetype, timestamp, camera, shutterspeedvalue, camerabearing, digitalzoomratio, exposuretime, focallength, 
+  const result = await env.SKYCHECK_DB.prepare(
+    `insert into images (bucket, path, mimetype, timestamp, camera, shutterspeedvalue, camerabearing, digitalzoomratio, exposuretime, focallength, 
       focallength35mm, gpsaltitude, gpshpositioningerror, gpsspeed, gpsspeedref, latitude, longitude, pixelheight, pixelwidth) values 
      (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+  )
+    .bind(
+      dataToSave.bucket,
+      dataToSave.fileName,
+      dataToSave.mimeType,
+      dataToSave.exifData.Timestamp,
+      dataToSave.exifData.Camera,
+      dataToSave.exifData.ShutterSpeedValue,
+      dataToSave.exifData.CameraBearing,
+      dataToSave.exifData.DigitalZoomRatio,
+      dataToSave.exifData.ExposureTime,
+      dataToSave.exifData.FocalLength,
+      dataToSave.exifData.FocalLength35mm,
+      dataToSave.exifData.GPSAltitude,
+      dataToSave.exifData.GPSHPositioningError,
+      dataToSave.exifData.GPSSpeed,
+      dataToSave.exifData.GPSSpeedRef,
+      dataToSave.exifData.Latitude,
+      dataToSave.exifData.Longitude,
+      dataToSave.exifData.PixelHeight,
+      dataToSave.exifData.PixelWidth
     )
-      .bind(
-        dataToSave.bucket,
-        dataToSave.fileName,
-        dataToSave.mimeType,
-        dataToSave.exifData.Timestamp,
-        dataToSave.exifData.Camera,
-        dataToSave.exifData.ShutterSpeedValue,
-        dataToSave.exifData.CameraBearing,
-        dataToSave.exifData.DigitalZoomRatio,
-        dataToSave.exifData.ExposureTime,
-        dataToSave.exifData.FocalLength,
-        dataToSave.exifData.FocalLength35mm,
-        dataToSave.exifData.GPSAltitude,
-        dataToSave.exifData.GPSHPositioningError,
-        dataToSave.exifData.GPSSpeed,
-        dataToSave.exifData.GPSSpeedRef,
-        dataToSave.exifData.Latitude,
-        dataToSave.exifData.Longitude,
-        dataToSave.exifData.PixelHeight,
-        dataToSave.exifData.PixelWidth
-      )
-      .run();
+    .run();
 
-    // console.log("result: ", result);
-
-    return new Response("success!");
-  // } catch (error) {
-  //   return new Response(error.message, { status: 500 });
-  // }
+  return new Response(JSON.stringify({ result }));
 };
